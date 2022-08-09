c28a02234a2c8bf4c177ca79bad389db39295cd4
Kapil Tyagi <kapill.tyagi@gmail.com>
2022-08-09 15:31:27 +0530

    Update

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -1,4 +1,26 @@
 package sort;
 
 public class BubbleSort {
+	
+	public static void main(String[] args) {
+		int[] a = {1,10,2,9,3,8,4,7,6,5};
+		sort(a);
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i]+" ");
+		}
+	}
+
+	private static void sort(int[] a) {
+		for (int i = 0; i < a.length-1; i++) {
+			for (int j = i+1; j < a.length; j++) {
+				if(a[i] > a[j]) {
+					int temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+			}
+		}
+		
+	}
+
 }
