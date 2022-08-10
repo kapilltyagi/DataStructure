187962878b8d704fbac22e01333bea1f9a8393dc
Kapil Tyagi <kapill.tyagi@gmail.com>
2022-08-10 13:24:09 +0530

    Update

--- src/array/KthLargestInArray.java
+++ src/array/KthLargestInArray.java
@@ -5,7 +5,6 @@ public class KthLargestInArray {
         int[] a ={1,10,2,9,3,8,4,7,6,5};
         int kth = 4;
         boolean flag = false;
-
         for (int i = 0; i < a.length-1 ; i++) {
             for (int j = i+1; j < a.length; j++) {
                 if(a[i] < a[j]){
